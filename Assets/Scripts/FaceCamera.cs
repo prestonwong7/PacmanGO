53925f662299f46974444247e995e0f9c8410123
prestonwong7 <prestonwong7@gmail.com>
2019-04-27 17:18:00 -0700

    Added settings icon, trying to add distance text to each epellet

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,37 @@
+﻿using Mapbox.Examples;
+using Mapbox.Unity.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Camera Camera;
+    private TextMeshPro distanceText;
+    private int distance = 0;
+    public PositionWithLocationProvider playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Camera == null)
+        {
+            this.transform.LookAt(Camera.main.transform.position);
+        }
+        else
+        {
+            this.transform.LookAt(Camera.transform.position);
+        }
+        playerPosition.GetComponent<PositionWithLocationProvider>();
+        float distance = Vector3.Distance(playerPosition._locationProvider.CurrentLocation.LatitudeLongitude.ToVector3xz(), this.gameObject.transform.localPosition.ToVector2d().ToVector3xz());
+        distanceText.text = "Distance: " + distance;
+    }
+}
