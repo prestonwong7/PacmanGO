5a709a5021d7295f5eeaf79fd68636f690e006a1
Ken Le <kle9597@gmail.com>
2019-05-01 19:27:05 -0700

    Music on/off now works

--- Assets/SoundController.cs
+++ Assets/SoundController.cs
@@ -5,26 +5,15 @@ using UnityEngine.UI;
 
 public class SoundController : MonoBehaviour
 {
-    GameObject onOffToggle;
-    bool result = false;
-
-    private void Start()
+    public void Toggle_Change(bool flag)
     {
-        onOffToggle = GameObject.Find("On/Off Toggle");
-        result = onOffToggle.GetComponent<Toggle>().isOn;
-        Debug.Log("Result is: " + result);
-        FindObjectOfType<AudioManager>().Stop("BG Music");
+        if(flag)
+        {
+            FindObjectOfType<AudioManager>().Play("BG Music");
+        }
+        else
+        {
+            FindObjectOfType<AudioManager>().Stop("BG Music");
+        }
     }
-
-    private void Update()
-    {
-        //Debug.Log("Result is: " + result);
-    }
-    //public void checkValue()
-    //{
-    //    bool result = onOffToggle.GetComponent<Toggle>().isOn;
-    //    Debug.Log("Result is: " + result);
-
-    //}
-
 }
